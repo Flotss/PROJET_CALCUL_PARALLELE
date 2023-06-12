cc37f4bce58ccd40283f81a476322cd10995027d
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-06-12 13:55:47 +0200

    chore: added some javadoc and removed comment into the server class

---
+++ src/Server.java
@@ -0,0 +1,22 @@
+import raytracer.Image;
+import raytracer.Scene;
+
+import java.rmi.RemoteException;
+
+public class Server implements ServiceRaytracer{
+
+    /**
+     * @param scene Scene to render
+     * @param width Width of the subscene
+     * @param height Height of the subscene
+     * @param x1 x coordinate of the subscene
+     * @param y1 y coordinate of the subscene
+     * @return Rendered Image from the subscene
+     * @throws RemoteException If the server is not available
+     */
+    @Override
+    public Image renderSubScene(Scene scene, int width, int height, int x1, int y1) throws RemoteException {
+        return scene.compute(x1, y1, width, height);
+    }
+
+}
